17d68a3d0eebf81ec0a96da1f95455a0cfa55f4c
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-05-16 18:26:36 -0700

    starting to separate calculations from display methods

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ var store_names = ['Hillsboro', 'Pearl', 'DowntownPDX', 'Buckman', 'PDXairport',
 var stores = [];
 var opening = 8;
 var hours_open = 18;
+var projected = [
+  [0, 4, 0, 4],
+  [0, 7, 0, 4],
+  [2, 15, 1, 4],
+  [15, 35, 3, 8],
+  [12, 31, 5, 12],
+  [5, 20, 5, 11]
+];
 
 // Store constructor
 var Store = function(name_input) {
   this.name = name_input;
 
   this.generate_random_pizzas = function(hour) {
-    var projected = [
-      [0, 4, 0, 4],
-      [0, 7, 0, 4],
-      [2, 15, 1, 4],
-      [15, 35, 3, 8],
-      [12, 31, 5, 12],
-      [5, 20, 5, 11]
-    ];
     // Possible pizzas made per hour depends on the range of the hour chosen
     var range_set = Math.floor(hour / 3);
     var range_this_hour = projected[range_set][1] - projected[range_set][0];
@@ -43,47 +43,52 @@ var Store = function(name_input) {
   };
 };
 
-var output_div = document.getElementById('stores_div');
-
 // Initialize store object array
 for (var i = 0; i < store_names.length; i++) {
   stores.push(new Store(store_names[i]));
+}
 
-  var store_header = document.createElement('h2');
-  store_header.appendChild(document.createTextNode(stores[i].name));
-  output_div.appendChild(store_header);
+display_tables();
 
-  var table_el = document.createElement('table');
-  var header_row = document.createElement('thead');
-  var table_row = document.createElement('tr');
-  var headers = ['Hour', 'Total Pizzas Made', 'Deliveries Made', 'Recommended Drivers'];
-  for (var j = 0; j < headers.length; j++) {
-    var th_col = document.createElement('th');
-    th_col.appendChild(document.createTextNode(headers[j]));
-    table_row.appendChild(th_col);
-  }
-  header_row.appendChild(table_row);
-  table_el.appendChild(header_row);
-
-  // build tbody
-  var tbody = document.createElement('tbody');
-  for (var hour = 0; hour < hours_open; hour++) {
-    var random_obj = stores[i].generate_random_pizzas(hour);
-    var time = new Date();
-    time.setHours(opening + hour);
+// Build tables
+function display_tables() {
+  var output_div = document.getElementById('stores_div');
+  for (var i = 0; i < store_names.length; i++) {
+    var store_header = document.createElement('h2');
+    store_header.appendChild(document.createTextNode(stores[i].name));
+    output_div.appendChild(store_header);
+    var table_el = document.createElement('table');
+    var header_row = document.createElement('thead');
     var table_row = document.createElement('tr');
-    var cells = [format_time(time), random_obj.pizzas, random_obj.deliveries, random_obj.drivers];
-    for (var j = 0; j < cells.length; j++) {
+    var headers = ['Hour', 'Total Pizzas Made', 'Deliveries Made', 'Recommended Drivers'];
+    for (var j = 0; j < headers.length; j++) {
       var th_col = document.createElement('th');
-      th_col.appendChild(document.createTextNode(cells[j]));
+      th_col.appendChild(document.createTextNode(headers[j]));
       table_row.appendChild(th_col);
     }
-    tbody.appendChild(table_row);
+    header_row.appendChild(table_row);
+    table_el.appendChild(header_row);
+
+    // build tbody
+    var tbody = document.createElement('tbody');
+    for (var hour = 0; hour < hours_open; hour++) {
+      var random_obj = stores[i].generate_random_pizzas(hour);
+      var time = new Date();
+      time.setHours(opening + hour);
+      var table_row = document.createElement('tr');
+      var cells = [format_time(time), random_obj.pizzas, random_obj.deliveries, random_obj.drivers];
+      for (var j = 0; j < cells.length; j++) {
+        var th_col = document.createElement('th');
+        th_col.appendChild(document.createTextNode(cells[j]));
+        table_row.appendChild(th_col);
+      }
+      tbody.appendChild(table_row);
+    }
+
+    table_el.appendChild(tbody);
+
+    output_div.appendChild(table_el);
   }
-
-  table_el.appendChild(tbody);
-
-  output_div.appendChild(table_el);
 }
 
 function format_time(time) {
