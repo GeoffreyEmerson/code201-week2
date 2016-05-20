db50275087a32c1faaeb50793b17a46a6c6cd765
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-05-20 00:06:58 -0700

    load order test for gmaps api

--- views/google-map.js
+++ views/google-map.js
@@ -1,4 +1,4 @@
-function initMap() {
+var initMap = function() {
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
     center: {lat: 45.5231, lng: -122.6765},
